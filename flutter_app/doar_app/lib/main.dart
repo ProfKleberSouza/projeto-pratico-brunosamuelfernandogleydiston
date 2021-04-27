a3d87422628eff1e7d8ed1e2e64efce23183709e
bkelsen <brukelsen@gmail.com>
2021-04-27 16:06:24 -0300

    update

--- flutter_app/doar_app/lib/main.dart
+++ flutter_app/doar_app/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:doar_app/widgets/login_page.dart';
 import 'package:doar_app/database/persistdata_core.dart';
-
-void main() => runApp(MyApp());
-
 // Chrome emulate: flutter run -d chrome 
+void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   // Dados persistidos (Persitent Data) com SQLite
   final dbUser = MyDatabase.instance;
@@ -13,12 +11,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Main', // O título da aplicação
       theme: ThemeData(
-        primarySwatch: Colors.blue),
-      // O tema utilizado, possui alguns outros atributos a serem
-      // configurados como primarySwatch, brightness, primaryColor
-      // e accentColor entre outros. No exemplo usamos apenas um
-      // primarySwatch é definindo a classe Colors, que implementa
-      // internamente as especificações de cores do Material Design.
+        primarySwatch: Colors.blue
+      ),
       home: Login(),
       /*
       initialRoute: '/',
@@ -28,10 +22,6 @@ class MyApp extends StatelessWidget {
         '/profile': (BuildContext context) => Profile()
       },
       */
-      // A primeira tela a ser mostrada no aplicativo deve ser 
-      // especificada dentro desse atributo. Telas flutuantes são 
-      // distribuídas em rotas, neste caso home representa "/". 
-      // Rotas podem ser configuradas usando o atributo routes;
     );
   }
 }
