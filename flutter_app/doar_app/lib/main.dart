ab6f28b55b098be1b0f9a8152288b45f2cc405f3
Fernando L <fernando@abas.online>
2021-04-27 23:04:07 -0300

    comentado: codigo inacabado causando erro no build

--- flutter_app/doar_app/lib/main.dart
+++ flutter_app/doar_app/lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:doar_app/widgets/login_page.dart';
-import 'package:doar_app/database/persistdata_core.dart';
+// import 'package:doar_app/database/persistdata_core.dart';
 
 void main() => runApp(MyApp());
 
 // Chrome emulate: flutter run -d chrome 
 class MyApp extends StatelessWidget {
   // Dados persistidos (Persitent Data) com SQLite
-  final dbUser = MyDatabase.instance;
+  // final dbUser = MyDatabase.instance;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
