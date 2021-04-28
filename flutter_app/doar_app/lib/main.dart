262e6a5e6473acb788187330d8bfc7dcfb3c27c9
bkelsen <brukelsen@gmail.com>
2021-04-28 19:50:29 -0300

    update

--- flutter_app/doar_app/lib/main.dart
+++ flutter_app/doar_app/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'package:doar_app/widgets/login_page.dart';
 import 'package:doar_app/widgets/profile_page.dart';
-// import 'package:doar_app/database/persistdata_core.dart';
+//import 'package:doar_app/database/persistdata_core.dart';
 
 // Chrome emulate: flutter run -d chrome
 // Theme colors:
