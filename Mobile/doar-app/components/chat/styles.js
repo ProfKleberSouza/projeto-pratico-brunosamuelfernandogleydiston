807886d71995bb4e2864343056f8d6ba01437fca
Fernando L <fernando@abas.online>
2021-05-09 23:34:10 -0300

    add: design inicial do chat

--- Mobile/doar-app/components/chat/styles.js
+++ Mobile/doar-app/components/chat/styles.js
@@ -1,33 +1,73 @@
 import { StyleSheet } from 'react-native';
+import Colors from '../../styles'
 
 export const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    list: {
+        paddingHorizontal: 17,
+    },
+    footer: {
+        flexDirection: 'row',
+        height: 60,
+        paddingHorizontal: 10,
+        padding: 5,
+    },
+    btnSend: {
+        backgroundColor: Colors.primary,
+        width: 40,
+        height: 40,
+        borderRadius: 360,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    iconSend: {
+        width: 30,
+        height: 30,
+        alignSelf: 'center',
+    },
+    inputContainer: {
+        borderBottomColor: '#F5FCFF',
+        backgroundColor: '#FFFFFF',
+        borderRadius: 30,
+        borderBottomWidth: 1,
+        height: 40,
+        flexDirection: 'row',
+        alignItems: 'center',
         flex: 1,
-        display: 'flex',
-        justifyContent: "center",
-        alignItems: "center"
+        marginRight: 10,
     },
-    textStyle: {
-        backgroundColor: 'black',
-        fontSize: 20,
-        color: 'white'
-    },
-    messageContainer: {
+    inputs: {
+        height: 40,
+        padding: 10,
+        backgroundColor: '#F5FCFF',
+        marginLeft: 16,
         flex: 1,
-        position: 'fixed',
-        bottom: 0,
-        width: '100%',
-        margin: 3,
-        flexDirection: 'row'
-
     },
-    messageInput: {
-        alignSelf: 'stretch',
-        width: '78%',
+    balloon: {
+        maxWidth: 250,
+        padding: 15,
+        borderRadius: 20,
     },
-    sendButton: {
-        height: '100%',
-        margin: 5,
-        width: '18%',
-    }
+    itemIn: {
+        alignSelf: 'flex-start'
+    },
+    itemOut: {
+        alignSelf: 'flex-end'
+    },
+    time: {
+        alignSelf: 'flex-end',
+        margin: 15,
+        fontSize: 12,
+        color: "#808080",
+    },
+    item: {
+        marginVertical: 14,
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: "#eeeeee",
+        borderRadius: 300,
+        padding: 5,
+    },
 });
