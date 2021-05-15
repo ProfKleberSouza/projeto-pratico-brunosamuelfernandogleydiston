bd9e77a24bca3f52d455970397678c054550f17b
bkelsen <brukelsen@gmail.com>
2021-05-14 22:55:31 -0300

    perfil muito loco

--- Mobile/doar-app/src/components/profile/index.js
+++ Mobile/doar-app/src/components/profile/index.js
@@ -15,140 +15,150 @@ export default class Profile extends Component {
       stateUf: '',
       description: '',
       number: '',
-      isUpdate: false
+      isUpdate: true
     }
-    //await firebase.auth().currentUser.updateProfile(this.state);
   }
+
+  //firebase.auth().currentUser.updateProfile(this.state);
+
+  componentDidUpdate() { }
+
   updateProfile = (val, prop) => {
     const state = this.state;
     state[prop] = val;
     this.setState(state);
   }
   render() {
-    return this.state.isUpdate === false ? (
-      <View style={styles.container}>
-        <View style={styles.header}></View>
-        <Image
-          style={styles.avatar}
-          source={{ uri: this.state.photo }}
-        />
-        <View style={styles.body}>
-          <View style={styles.bodyContent}>
-            <Text
-              style={styles.userName}>
-              Nome Ultimo Nome
-                  {this.state.firstName}&nbsp;{this.state.lastName}
-            </Text>
-            <Text
-              style={styles.userDescription}>
-              Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,
-                  {this.state.description}
-            </Text>
-            <Text
-              style={styles.userInfo}>
-              Email@mail.com
-                  {this.state.email}
-            </Text>
-            <Text
-              style={styles.userInfo}>
-              +55 31 98563-4556
-                  {this.state.number}
-            </Text>
-            <Text
-              style={styles.userInfo}>
-              Cidade Estado
-                  {this.state.city}&nbsp;{this.state.stateUf}
-            </Text>
-            <Button
-              mode='contained'
-              uppercase={false}
-              style={styles.button}
-              labelStyle={{ color: '#ffffff', fontSize: 16, fontWeight: '600', }}
-              onPress={() => this.state.isUpdate = true}>
-              Atualizar
-              </Button>
-          </View>
-        </View>
-      </View>
-    ) : (
-      <View style={styles.container}>
-        <View style={styles.header}></View>
-        <Image
-          style={styles.avatar}
-          source={{ uri: this.state.photo }}
-        />
-        <View style={styles.body}>
-          <View style={styles.bodyContent}>
-            <TextInput
-              mode='flat'
-              style={styles.userName}
-              label='Primeiro Nome'
-              value={this.state.firstName}
-              onChangeText={(val) => this.updateProfile(val, 'firstName')}>
-              <TextInput
-                mode='flat'
-                style={styles.userName}
-                label='Último Nome'
-                value={this.state.lastName}
-                onChangeText={(val) => this.updateProfile(val, 'lastName')}>
-                &nbsp;
-              </TextInput>
-            </TextInput>
-            <TextInput
-              mode='flat'
-              multiline='true'
-              numberOfLines='5'
-              style={styles.userDescription}
-              label='Descrição'
-              value={this.state.description}
-              onChangeText={(val) => this.updateProfile(val, 'description')}>
-            </TextInput>
-            <TextInput
-              mode='flat'
-              style={styles.userInfo}
-              label='E-mail'
-              value={this.state.email}
-              onChangeText={(val) => this.updateProfile(val, 'email')}>
-            </TextInput>
-            <TextInput
-              mode='flat'
-              style={styles.userInfo}
-              label='Número de contato'
-              render={props =>
-                <TextInputMask
-                  {...props}
-                  mask="+[00] [00] [00000] [0000]"
-                  value={this.state.number}
-                  onChangeText={(val) => this.updateProfile(val, 'number')}>
-                </TextInputMask>
-              }>
-            </TextInput>
-            <TextInput
-              mode='flat'
-              style={styles.userInfo}
-              label='Cidade'
-              value={this.state.city}
-              onChangeText={(val) => this.updateProfile(val, 'city')}>
-              &nbsp;
-              <TextInput
-                mode='flat'
-                style={styles.userInfo}
-                label='Estado'
-                value={this.state.stateUf}
-                onChangeText={(val) => this.updateProfile(val, 'stateUf')}>
-              </TextInput>
-            </TextInput>
-            <Button
-              mode='contained'
-              uppercase={false}
-              style={styles.button}
-              labelStyle={{ color: '#ffffff', fontSize: 16, fontWeight: '600', }}
-              onPress={() => this.state.isUpdate = false}>
-              Salvar
-              </Button>
-          </View>
-        </View>
-      </View>
-    )
+    const isUpdate = this.state.isUpdate;
+    return (
+      <div>
+        { isUpdate
+          ? (
+            <View style={styles.container}>
+              <View style={styles.header}></View>
+              <Image
+                style={styles.avatar}
+                source={{ uri: this.state.photo }}
+              />
+              <View style={styles.body}>
+                <View style={styles.bodyContent}>
+                  <Text
+                    style={styles.userName}>
+                    Nome Ultimo Nome
+                    {this.state.firstName}&nbsp;{this.state.lastName}
+                  </Text>
+                  <Text
+                    style={styles.userDescription}>
+                    Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,
+                    {this.state.description}
+                  </Text>
+                  <Text
+                    style={styles.userInfo}>
+                    Email@mail.com
+                    {this.state.email}
+                  </Text>
+                  <Text
+                    style={styles.userInfo}>
+                    +55 31 98563-4556
+                    {this.state.number}
+                  </Text>
+                  <Text
+                    style={styles.userInfo}>
+                    Cidade Estado
+                    {this.state.city}&nbsp;{this.state.stateUf}
+                  </Text>
+                  <Button
+                    mode='contained'
+                    uppercase={false}
+                    style={styles.button}
+                    labelStyle={{ color: '#ffffff', fontSize: 16, fontWeight: '600', }}
+                    onPress={() => this.setState({ isUpdate: false })}>
+                    Atualizar
+                </Button>
+                </View>
+              </View>
+            </View>
+          ) : (
+            <View style={styles.container}>
+              <View style={styles.header}></View>
+              <Image
+                style={styles.avatar}
+                source={{ uri: this.state.photo }}
+              />
+              <View style={styles.body}>
+                <View style={styles.bodyContent}>
+                  <TextInput
+                    mode='flat'
+                    style={styles.userEdit}
+                    label='Imagem URL'
+                    value={this.state.phot}
+                    onChangeText={(val) => this.updateProfile(val, 'photo')}>
+                  </TextInput>
+                  <TextInput
+                    mode='flat'
+                    style={styles.userEdit}
+                    label='Primeiro nome'
+                    value={this.state.firstName}
+                    onChangeText={(val) => this.updateProfile(val, 'firstName')}>
+                  </TextInput>
+                  <TextInput
+                    mode='flat'
+                    style={styles.userEdit}
+                    label='Último nome'
+                    value={this.state.lastName}
+                    onChangeText={(val) => this.updateProfile(val, 'lastName')}>
+                  </TextInput>
+                  <TextInput
+                    mode='flat'
+                    multiline='true'
+                    numberOfLines='4'
+                    style={styles.userEdit}
+                    label='Descrição'
+                    value={this.state.description}
+                    onChangeText={(val) => this.updateProfile(val, 'description')}>
+                  </TextInput>
+                  <TextInput
+                    mode='flat'
+                    style={styles.userEdit}
+                    label='E-mail'
+                    value={this.state.email}
+                    onChangeText={(val) => this.updateProfile(val, 'email')}>
+                  </TextInput>
+                  <TextInput
+                    mode='flat'
+                    style={styles.userEdit}
+                    label='Número de contato'
+                    value={this.state.number}
+                    onChangeText={(val) => this.updateProfile(val, 'number')}>
+                  </TextInput>
+                  <TextInput
+                    mode='flat'
+                    style={styles.userEdit}
+                    label='Cidade'
+                    value={this.state.city}
+                    onChangeText={(val) => this.updateProfile(val, 'city')}>
+                  </TextInput>
+                  <TextInput
+                    mode='flat'
+                    style={styles.userEdit}
+                    label='Estado'
+                    value={this.state.stateUf}
+                    onChangeText={(val) => this.updateProfile(val, 'stateUf')}>
+                  </TextInput>
+                  <Button
+                    mode='contained'
+                    uppercase={false}
+                    style={styles.button}
+                    labelStyle={{ color: '#ffffff', fontSize: 16, fontWeight: '600', }}
+                    onPress={() => this.setState({ isUpdate: true })}>
+                    Salvar
+                  </Button>
+                </View>
+              </View>
+            </View>
+          )
+        }
+      </div>
+    );
   }
 }
